14bccc6dca7598492f1e8940ebc46aa16359ed85
Farhad Qaunain <fqqaunin@gmail.com>
2026-01-14 00:59:37 +0530

    Fix import statement for addListing controller in listing router

--- backend/routes/listing.js
+++ backend/routes/listing.js
@@ -2,6 +2,7 @@ const express = require('express');
 const listingRouter = express.Router();
 const isAuth = require('../middleware/isAuth.js');
 const upload = require('../middleware/multer.js');
+const { addListing } = require('../controllers/listing.js');
 
 listingRouter.post('/add', isAuth, upload.fields(
     [{ name: 'image1', maxCount: 1 },
