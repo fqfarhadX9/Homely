46ecfefe90f268ae7122cafdf1a885f7e16ce7d3
Farhad Qaunain <fqqaunin@gmail.com>
2026-01-27 02:58:03 +0530

    added frontend url in index

--- backend/index.js
+++ backend/index.js
@@ -26,5 +26,5 @@ app.use("/api/booking", bookingRouter);
 
 app.listen(port, () => {
     connectDB();
-    console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
