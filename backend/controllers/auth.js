74f139d7b7b62b9bcd48c42e1baa5bdda2d621ff
Farhad Qaunain <fqqaunin@gmail.com>
2026-01-27 04:23:28 +0530

    fixes bugs in auth controller

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -32,8 +32,8 @@ const Signup = async (req, res) => {
 
     res.cookie("token", token, {
     httpOnly: true,
-    sameSite: "lax",
-    secure: false,
+    secure: true,
+    sameSite: "none",
     maxAge: 7 * 24 * 60 * 60 * 1000,
   });
 
@@ -78,7 +78,7 @@ const Signin = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: true,
-      sameSite: "lax",
+      sameSite: "none",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
