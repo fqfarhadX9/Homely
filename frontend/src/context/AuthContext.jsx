02856abfbfecf7b62e0ece3204f163cc571ab7b7
Farhad Qaunain <fqqaunin@gmail.com>
2026-01-26 23:27:46 +0530

    added backend url in authcontext

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { AuthDataContext } from "./AuthDataContext"; 
 
 function AuthContext({ children }) {
-    const serverUrl = "http://localhost:8000";
+    const serverUrl = "https://homely-backend-eptt.onrender.com";
     const [loading, setLoading] = useState(false);
     const value = {
         serverUrl,
